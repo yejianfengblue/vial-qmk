aed0b9d2767b5e89e51e52a0850f749a9289eb6e
Ken@X-Bows <k@x-bows.com>
2022-07-05 11:16:00 +0800

    Add knight and knight plus vial for K

---
+++ keyboards/xbows/knight_plus/keymaps/vial-k/rules.mk
@@ -0,0 +1,30 @@
+# See https://docs.qmk.fm/#/squeezing_avr?id=rulesmk-settings about reducing firmware size
+
+LTO_ENABLE = yes # link time optimization
+
+GRAVE_ESC_ENABLE = no # 140
+
+SPACE_CADET_ENABLE = no # 380
+
+EXTRAKEY_ENABLE = no       # Audio control and System control
+
+NKRO_ENABLE = no # 366
+
+# magic keycode, including toggle N-key rollover, https://docs.qmk.fm/#/keycodes_magic
+MAGIC_ENABLE = no # 628 byte
+
+# emulate mouse click and move
+MOUSEKEY_ENABLE = yes # 1352 byte
+
+KEY_OVERRIDE_ENABLE = no  # 2000 byte
+
+TAP_DANCE_ENABLE = yes # 2000 byte
+
+COMBO_ENABLE = no # 2500 byte
+
+QMK_SETTINGS = yes # 3300 byte
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# RGB_MATRIX_ENABLE = yes
